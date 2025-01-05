c0cd3e00eb8110817cb58a7511b336565d76a3ce
yeira.putri <yeira.putri@cs.ui.ac.id>
2025-01-05 21:00:31 +0700

    fix deploy error

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   }
+  
 };
 
 export default nextConfig;
